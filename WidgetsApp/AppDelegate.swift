2e765fd0c6deb34504fec62653bf17feabfec2a6
Матвеев Роман Станиславович <Matveev.R.St@sberbank.ru>
2020-10-08 19:38:07 +0300

    Initial Commit

---
+++ WidgetsApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  WidgetsApp
+//
+//  Created by Roman Matveev on 08.10.2020.
+//  Copyright © 2020 BeornStudio. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+}
+
---
+++ WidgetsApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WidgetsApp
+//
+//  Created by Roman Matveev on 08.10.2020.
+//  Copyright © 2020 BeornStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
